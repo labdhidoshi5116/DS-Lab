7a5cac5d0392cb3d794be41a723bfadb6fbc62fe
Labdhi Doshi <labdhidoshi5116@gmail.com>
2025-07-24 10:35:40 +0530

    new

---
+++ Lab-6/prog37.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main(){
+    int n;
+    printf("Enter the number of test cases:");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        
+
+    }
+}
